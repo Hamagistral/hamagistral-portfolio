24c0b824d8b5796c9a8c0081cc92a07a8c6028b9
hamagistral <hamza.lbelghiti@gmail.com>
2025-03-19 02:15:25 +0100

    updated resume & added 3d model

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,4 +171,79 @@ themeButton.addEventListener('click', () => {
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
-})   
+})
+
+/*==================== PARTICLES ====================*/
+particlesJS('particles-js',
+{
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#44a5e9"
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#44a5e9",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        }
+    },
+    "retina_detect": true
+});
+
+/*==================== AOS INITIALIZATION ====================*/
+AOS.init({
+    duration: 1000,
+    once: true,
+    offset: 100
+});
+
+/*==================== TYPED JS INITIALIZATION ====================*/
+const typed = new Typed('.typed-text', {
+    strings: ['👋🏻 Hi, I\'m Hamza', 'I\'m a Analytics and AI Engineer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+});   
